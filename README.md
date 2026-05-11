419f3bbcc0e64702d623a4260648a7d06c48037f
fabetabilo <fab.tabilo@duocuc.cl>
2026-05-11 15:34:35 -0400

    add: proyecto inicial
    Implementacion de libreria ui compartida

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Navium | Vista de usuario Operario de Patio
+
+### Requisitos
+
+- Node.js 18+ (recomendado LTS). Verifica versión:
+
+```powershell
+node -v
+npm -v
+```
+Descarga e instala desde: https://nodejs.org/
+
+### Preparar Entorno de Desarrollo
+
+Instalar dependencias
+```
+npm install
+```
+```
+npm install react-router-dom
+```
+
+Libreria UI compartida [navium-ui-lib](https://github.com/fabetabilo/navium-ui-lib.git) (v2.0.0)
+```
+npm install navium-ui-lib
+```
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'navium-ui-lib';
+import logo from '../assets/navium-v1.png';
+import './Navbar.css';
+
+function Navbar() {
+	return (
+		<header className="navbar">
+			<div className="navbar__inner">
+				<a className="navbar__brand" href="#home" aria-label="Navium">
+					<img src={logo} alt="Navium" />
+				</a>
+				<nav className="navbar__links" aria-label="Primary">
+					<a href="#servicios">Servicios</a>
+					<a href="#nosotros">Nosotros</a>
+					<a href="#clientes">Clientes</a>
+					<a href="#contacto">Contacto</a>
+					<Button className="navbar__button" variant="primary">Iniciar Sesión</Button>
+				</nav>
+			</div>
+		</header>
+	);
+}
+
+export default Navbar;
